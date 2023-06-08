9b1221831407bb0031bc6e65a2ed4aa983a36c52
fmora122 <pfmoras@gmail.com>
2023-06-07 19:07:04 -0600

    first draft

---
+++ src/clase3/Clase3.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase3;
+
+/**
+ *
+ * @author fmora
+ */
+public class Clase3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("@@@@@@@@@@");
+        }
+        System.out.println("SEGUNDO INTENTO");
+        
+        
+        String dispBlock;
+        String test1;
+        test1 = "";
+        dispBlock = "@@@@@@@@@@\n";
+        for (int i = 0; i <= 5; i++) {
+            test1 = test1+dispBlock;
+        }
+        System.out.println(test1);
+    }     
+  
+}
+
